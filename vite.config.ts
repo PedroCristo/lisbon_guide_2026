86943f3af25abaa29c27a59085d2c4e1bd5df267
Pedro Cristo <pcristo35@gmail.com>
2026-04-09 11:21:07 +0100

    Run build for prodcuion

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,31 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
+import sitemap from 'vite-plugin-sitemap';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
-    plugins: [react(), tailwindcss()],
+    plugins: [
+      react(),
+      tailwindcss(),
+     sitemap({
+      hostname: 'https://lisbonguide.site',
+      dynamicRoutes: [
+        '/',
+        '/explore',
+        '/where-to-eat',
+        '/around-lisbon',
+        '/cascais/see',
+        '/cascais/eat',
+        '/sintra/see',
+        '/sintra/eat',
+        '/about',
+        '/contact',
+      ],
+      exclude: ['/google1a4d2e863c003d4a'],
+    }),
+    ],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
     },
