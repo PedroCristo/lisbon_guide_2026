af8b763858430b927f614adbc81477525915aab5
Pedro Cristo <pcristo35@gmail.com>
2026-07-04 12:38:33 +0100

    Change sitemap

--- vite.config.ts
+++ vite.config.ts
@@ -2,37 +2,26 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
-import sitemap from 'vite-plugin-sitemap';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
+
   return {
     plugins: [
       react(),
       tailwindcss(),
-     sitemap({
-      hostname: 'https://lisbonguide.site',
-      dynamicRoutes: [
-        '/explore',
-        '/where-to-eat',
-        '/cascais/see',
-        '/cascais/eat',
-        '/sintra/see',
-        '/sintra/eat',
-        '/about',
-        '/contact',
-      ],
-      exclude: ['/google1a4d2e863c003d4a'],
-    }),
     ],
+
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
     },
+
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
       },
     },
+
     server: {
       hmr: process.env.DISABLE_HMR !== 'true',
     },
